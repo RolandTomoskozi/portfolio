f07963a3cae8795613912c3df349b5fda0ebbde1
rtoemoeskoezi <rolandtomoskozi@gmail.com>
2021-10-01 16:32:24 +0200

    update startpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'portfolio';
+
+  testText$ : Observable<any>;
+
+  public constructor(protected http: HttpClient) {
+    const headers = new HttpHeaders({
+      Accept: 'application/vnd.github.v3+json'
+    });
+    this.testText$ = this.http.get('https://api.github.com/users/RolandTomoskozi/repos', {headers})
+  }
+
+  ngOnInit(): void {
+
+
+
+  }
 }
