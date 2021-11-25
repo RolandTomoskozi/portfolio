d4e4d793d8a74089765fbd78ebfe061e70f36119
rtoemoeskoezi <rolandtomoskozi@gmail.com>
2021-11-25 17:20:26 +0100

    init firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
+import firebase from "firebase/compat";
 
 @Component({
   selector: 'app-root',
@@ -20,8 +21,14 @@ export class AppComponent implements OnInit{
   }
 
   ngOnInit(): void {
-
-
-
+    firebase.initializeApp({
+      apiKey: "AIzaSyB_eJw2HxmSLSIwSDH9FjmwqffadFMxoU8",
+      authDomain: "portfolio-bd11d.firebaseapp.com",
+      projectId: "portfolio-bd11d",
+      storageBucket: "portfolio-bd11d.appspot.com",
+      messagingSenderId: "679069574719",
+      appId: "1:679069574719:web:e04ad3b6d7608080663a8a",
+      measurementId: "G-GLQHLW16FL"
+    });
   }
 }
